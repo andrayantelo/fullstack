045aed6de9f8e32d3384cb0b316f1eba46d084db
andrayantelo <andrayantelo@gmail.com>
2020-04-06 16:28:24 -0700

    new functions for component

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,10 +12,10 @@ const Button = ({name, handleClick}) => {
     )
 }
 
-const Display = ({ name, feedback }) => {
+const Display = ({ name, data }) => {
     return (
         <div>
-            {name} {feedback}
+            {name} {data}
         </div>
     )
 }
@@ -25,9 +25,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const state = [good, neutral, bad];
   
   const handleClick = (changeState) => () => {
       changeState(prevState => prevState + 1);
+  }
+
+  const total = () => [good, neutral, bad].reduce((a,b) => a + b);
+  const avg = () => {
+      // (good: 1, neutral: 0, bad: -1)
+      if (total() === 0) {
+          return 0;
+      }
+      return ((good*1 + neutral*0 + bad*-1)/total());
+  }
+  const positive = () => {
+      if (total() === 0) {
+          return 0;
+      }
+      return good/total();
   }
 
   return (
@@ -37,9 +54,12 @@ const App = () => {
         <Button name={"neutral"} handleClick={handleClick(setNeutral)} />
         <Button name={"bad"} handleClick={handleClick(setBad)} />
       <h1>Statistics</h1>
-        <Display name={"good"} feedback={good} />
-        <Display name={"neutral"} feedback={neutral} />
-        <Display name={"bad"} feedback={bad} />
+        <Display name={"good"} data={good} />
+        <Display name={"neutral"} data={neutral} />
+        <Display name={"bad"} data={bad} />
+        <Display name={"all"} data={total()} />
+        <Display name={"average"} data={avg()} />
+        <Display name={"positive"} data={positive() + "%"} />
      </div>
   )
 }
