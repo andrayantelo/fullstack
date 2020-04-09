b0c9e918fdf84885b79a9275e2d2fefdf617b1cc
andrayantelo <andrayantelo@gmail.com>
2020-04-09 14:21:15 -0700

    final exercises of part1

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleClick}>Click Me</button>
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const handleClick = () => {
+      const index = getRandomInt(5);
+      setSelected(index);
+  }
+
+  return (
+    <div>
+      {props.anecdotes[selected]}
+      <Button handleClick={handleClick} />
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
