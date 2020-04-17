c6d29b73713b154e54729cfdedd76d6aa256d4f3
andrayantelo <andrayantelo@gmail.com>
2020-04-17 14:09:53 -0700

    Added a Notification component that gets rendered if there is a server error.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Notification from './components/Notification';
 import personsService from './services/persons';
 
 
@@ -10,16 +11,25 @@ const App = () => {
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ newFilter, setNewFilter ] = useState('');
+  const [ errorMessage, setErrorMessage ] = useState(null);
 
-  useEffect(() => {
-      personsService.getAll()
-      .then(personsData => setPersons(personsData))
-      .catch(error => console.log("could not get persons data", error));
-  }, [])
+
+    const emitError = (message) => {
+        setErrorMessage(message);
+        setTimeout(() => setErrorMessage(null), 5000);
+    }
+
+    useEffect(() => {
+        personsService.getAll()
+        .then(personsData => setPersons(personsData))
+        .catch(error => {
+            emitError(`Could not get persons data. Error: ${error}`)
+        });
+    }, [])
 
     const existing = (name) => {
         // returns true if person with name 'name' already in personss
-        return persons.filter(person => person.name === name);
+        return persons.filter(person => person.name === name).length;
     }
 
     const clearForm = () => {
@@ -37,7 +47,9 @@ const App = () => {
                 setPersons(persons.filter(p => p.id !== person.id).concat(updatedPerson));
                 clearForm();
             })
-            .catch(error => console.error(error));
+            .catch(error => {
+                emitError(`Could not update ${person.name}'s data. Error: ${error}`);
+            });
     }
 
     const addPerson = (event) => {
@@ -56,68 +68,71 @@ const App = () => {
                 clearForm();
             })
             .catch(error => {
-                alert(`${newName} is already added to the phonebook`);
+                emitError(`${newName} is already in phonebook. Error: ${error}`);
             })
         }
-  }
+    }
   
-  const handleDelete = (person) => {
-    if (window.confirm(`Delete ${person.name}?`)) {
-        personsService.deletePerson(person.id)
-          .then(response => {
-              setPersons(persons.filter(person => person.id !== response.id));
-          })
-          .catch(error => console.log("an error occurred: ", error));  
-      }
-  }
+    const handleDelete = (person) => {
+        if (window.confirm(`Delete ${person.name}?`)) {
+            personsService.deletePerson(person.id)
+                .then(response => {
+                    setPersons(persons.filter(person => person.id !== response.id));
+                })
+                .catch(error => {
+                    emitError(`Could not delete ${person.name}. Error: ${error}`);
+                });  
+        }
+    }
 
-  const handleNameChange = (event) => {
-      setNewName(event.target.value);
-  }
+    const handleNameChange = (event) => {
+        setNewName(event.target.value);
+    }
   
-  const handleNumberChange = (event) => {
-      setNewNumber(event.target.value);
-  }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
+    }
 
-  const handleFilterChange = (event) => {
-      setNewFilter(event.target.value);
-  }
+    const handleFilterChange = (event) => {
+        setNewFilter(event.target.value);
+    }
 
-  const filterPeople = persons.filter((person) => {
-      return person.name.includes(newFilter);
-  })
+    const filterPeople = persons.filter((person) => {
+        return person.name.includes(newFilter);
+    })
 
-  const renderPeople = (arr) => {
-  return arr.map((person) => {
-      return (
-          <div key={person.id}>
-               {person.name} {person.number}
-               <button onClick={() => handleDelete(person)} value={person.id}>delete</button>
-          </div>
-      )
-      });
-  } 
-  return (
+    const renderPeople = (arr) => {
+    return arr.map((person) => {
+        return (
+            <div key={person.id}>
+                {person.name} {person.number}
+                <button onClick={() => handleDelete(person)} value={person.id}>delete</button>
+            </div>
+        )
+        });
+    } 
+    return (
     <div>
-      <h2>Phonebook</h2>
-          <Filter handleChange={handleFilterChange} newFilter={newFilter} />
-      <h2>Add a new</h2>
-          <PersonForm
-              addPerson={addPerson}
-              newName={newName}
-              handleNameChange={handleNameChange}
-              newNumber={newNumber}
-              handleNumberChange={handleNumberChange}
-          />
-      <h2>Numbers</h2>
-          <Persons
-              newFilter={newFilter}
-              renderPeople={renderPeople}
-              filterPeople={filterPeople}
-              persons={persons}
-          />
+        <Notification message={errorMessage} />
+        <h2>Phonebook</h2>
+            <Filter handleChange={handleFilterChange} newFilter={newFilter} />
+        <h2>Add a new</h2>
+            <PersonForm
+                addPerson={addPerson}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newNumber={newNumber}
+                handleNumberChange={handleNumberChange}
+            />
+        <h2>Numbers</h2>
+            <Persons
+                newFilter={newFilter}
+                renderPeople={renderPeople}
+                filterPeople={filterPeople}
+                persons={persons}
+            />
     </div>
-  )
+    )
 }
 
 export default App
